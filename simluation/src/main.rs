cfe32ac539d64bbfb728d5e336436637b96c40e7
GStudy2010 <gs.live.study@gmail.com>
2026-05-28 00:34:47 +0000

    feat: Nginx and cloudflare tunneling for https whole internet connection.

---
+++ simluation/src/main.rs
@@ -0,0 +1,40 @@
+use std::net::SocketAddr;
+use axum::routing::post;
+use serde::{Deserialize, Serialize};
+use axum::{Json, Router, http::StatusCode, response::IntoResponse};
+use tower_http::cors::CorsLayer;
+
+#[derive(Debug, Deserialize)]
+struct TestRouteHandlerRequest {
+    test: String,
+}
+#[derive(Debug, Serialize)]
+struct TestRouteHandlerResponse{
+    test: String,
+}
+
+async fn test_route_handler(
+    Json(payload): Json<TestRouteHandlerRequest>,
+    ) -> impl IntoResponse {
+    println!("Test route has been called, the message: {:?}", payload.test);
+    let resp = TestRouteHandlerResponse {
+        test: "Test".to_string(),
+    };
+    (StatusCode::OK, Json(resp))
+}
+
+#[tokio::main]
+async fn main() {
+    let server = Router::new()
+        .route("/", post(test_route_handler))
+        .layer(CorsLayer::permissive());
+    let addr = SocketAddr::from(([127, 0, 0, 1], 42069));
+
+    println!("Server started");
+
+    let listener = tokio::net::TcpListener::bind(addr)
+        .await
+        .unwrap();
+    axum::serve(listener, server).await.unwrap();
+    tokio::signal::ctrl_c().await.unwrap();
+}
