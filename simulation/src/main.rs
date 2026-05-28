94de7717559e552cc71181d222cd378b87d6d347
GStudy2010 <gs.live.study@gmail.com>
2026-05-28 02:05:27 +0000

    feat: Connected postgres database to server

--- simulation/src/main.rs
+++ simulation/src/main.rs
@@ -3,6 +3,12 @@ use axum::routing::post;
 use serde::{Deserialize, Serialize};
 use axum::{Json, Router, http::StatusCode, response::IntoResponse};
 use tower_http::cors::CorsLayer;
+use sqlx::{PgPool, postgres::PgPoolOptions};
+
+#[derive(Clone)]
+pub struct AppState {
+    pub db: PgPool,
+}
 
 #[derive(Debug, Deserialize)]
 struct TestRouteHandlerRequest {
@@ -23,8 +29,22 @@ async fn test_route_handler(
     (StatusCode::OK, Json(resp))
 }
 
+async fn connect_db(database_url: &str) -> PgPool {
+    PgPoolOptions::new()
+        .max_connections(10)
+        .connect(database_url)
+        .await
+        .expect("Failed to connect")
+}
+
 #[tokio::main]
 async fn main() {
+    dotenvy::dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").unwrap();
+    let pool = connect_db(&database_url).await;
+    let _state = AppState {
+        db: pool,
+    };
     let server = Router::new()
         .route("/api/test", post(test_route_handler))
         .layer(CorsLayer::permissive());
